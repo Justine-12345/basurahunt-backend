21c411e92a16c6910324d8c9bb6d18fd9526a42a
unknown <castanedajustine09@gmail.com>
2022-11-21 21:53:38 +0800

    first commit

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin, token");
+  next();
+});
 
 app.get('/api/test', (req, res) => {
   res.send('congrats');
